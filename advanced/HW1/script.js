b50cd92c2b8432a487b1cb528fb7935abe912606
Laance <andriyblack4@gmail.com>
2021-09-27 12:33:54 +0300

    HW1 fixed v3

--- advanced/HW1/script.js
+++ advanced/HW1/script.js
@@ -1,30 +1,44 @@
-let radioPrice = 15.678;
-let waterPricee = 123.965;
-let fuelPrice = 90.2345;
-let maxPrice = Math.max(radioPrice, waterPricee, fuelPrice);
-let minPrice = Math.min(radioPrice, waterPricee, fuelPrice);
-let sumPrice = radioPrice + waterPricee + fuelPrice;
-let radioRounded = Math.floor(radioPrice);
-let waterRounded = Math.floor(waterPricee);
-let fuelRounded = Math.floor(fuelPrice);
-let sumOfRounding = radioRounded + waterRounded + fuelRounded;
-let roundedToHundreds = Math.round(sumPrice / 100) * 100;
-let checkIfEven = sumOfRounding% 2 == 0;
-let money = 500;
-let sumRest = money - sumPrice;
-let avrgPrice = (radioPrice + waterPricee + fuelPrice) / 3;
-let roundingEverage = Math.round(avrgPrice * 100) / 100;
-let discount = Math.floor(Math.random() * 101);
-let discountSum = (sumPrice * (1 - discount/100)).toFixed(2);
-let profit = ((sumPrice / 2) - ((discount / 100) * sumPrice)).toFixed(2);
+const radioPrice = 15.678;
+const waterPrice = 123.965;
+const fuelPrice = 90.2345;
+const money = 500;
+
+const maxPrice = Math.max(radioPrice, waterPrice, fuelPrice);
+const minPrice = Math.min(radioPrice, waterPrice, fuelPrice);
+const sumPrice = radioPrice + waterPrice + fuelPrice;
+
+const radioRounded = Math.floor(radioPrice);
+const waterRounded = Math.floor(waterPrice);
+const fuelRounded = Math.floor(fuelPrice);
+const sumOfRounding = radioRounded + waterRounded + fuelRounded;
+const roundedToHundreds = Math.round(sumPrice / 100) * 100;
+
+const checkIfEven = sumOfRounding% 2 == 0;
+const sumRest = money - sumPrice;
+const avrgPrice = sumOfRounding / 3;
+const roundingEverage = Math.round(avrgPrice * 100) / 100;
+
+const discount = Math.floor(Math.random() * 101);
+const discountSum = (sumPrice * (1 - discount/100)).toFixed(2);
+const profit = ((sumPrice / 2) - ((discount / 100) * sumPrice)).toFixed(2);
 console.log(`Max price: ${maxPrice}
+
 Min price: ${minPrice}
+
 Total price: ${sumPrice}
+
 Sum of Rounded prices: ${sumOfRounding}
+
 Rounded to hundreds: ${roundedToHundreds}
+
 Total amount of prices is even: ${checkIfEven}
+
 Rest: ${sumRest}
+
 Everage: ${roundingEverage}
+
 Discount: ${discount}
+
 Total amount with discount: ${discountSum}
+
 Profit: ${profit}`);
