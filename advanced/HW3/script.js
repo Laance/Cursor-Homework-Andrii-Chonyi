e28cfa02125b0f22036f56c5faa7b163e83cd04a
Laance <andriyblack4@gmail.com>
2021-10-04 21:50:10 +0300

    HW3 fixed v3

--- advanced/HW3/script.js
+++ advanced/HW3/script.js
@@ -7,10 +7,21 @@ const getPow = (n,pow) => {
     let result = 1;
     if(pow===0){
         return result = 1;
+    } 
+    else  if (pow > 0){
+        for(let i = 1; i <= pow ; i++){
+            result *= n;
+        } 
     }
-    for(let i = 1; i <= pow ; i++){
-        result *= n;
+    else if (pow < 0){
+        tempResult = 1;
+        for(let i = -1; i >= pow ; i--){
+            tempResult *= n;
+            tempResult2 = 1/tempResult;
+            result = tempResult2.toFixed(4); 
+        }          
     }
+
     return result;
 }
 
@@ -38,7 +49,7 @@ const repitsOfLetter = (letter, word) => {
 document.writeln(
     `
     Функція№1: Максимальне число з 25498 - ${getMaxDigit(25498)}</br>
-    Функція№2: Корінь з 3^3 - ${getPow(3,3)}</br>
+    Функція№2: Корінь з 3^3 - ${getPow(3,-3)}</br>
     Функція№3: Виправлення імені з АНДРІЙ - ${correctName('АНДРІЙ')}</br>
     Функція№4: Зарплата з 14000 і податком у 19.5% - ${getMoney(14000)}</br>
     Функція№5: Рандомне число з 2 по 10 - ${getRandom(2,10)}</br>
