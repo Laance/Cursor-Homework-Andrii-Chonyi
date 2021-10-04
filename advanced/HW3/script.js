ff68504dd523beb35aea65661b755937dc019724
Laance <andriyblack4@gmail.com>
2021-10-04 18:30:17 +0300

    HW3 fixed v2

--- advanced/HW3/script.js
+++ advanced/HW3/script.js
@@ -1,23 +1,26 @@
-const getMaxDigit = (number)=>{
+const getMaxDigit = (number) => {
     const arr = number.toString().split(``);
         return Math.max(...arr);
 }
 
-const getPow = (n,pow)=>{
-    let result = n;
-    for(let i=1; i<n;i++){
-        result*=n;
+const getPow = (n,pow) => {
+    let result = 1;
+    if(pow===0){
+        return result = 1;
+    }
+    for(let i = 1; i <= pow ; i++){
+        result *= n;
     }
     return result;
 }
 
 const correctName = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-const getMoney = (money)=>{
-    return  money-(money*(19.5/100));
+const getMoney = (money) => {
+    return  money - (money * (19.5 / 100));
 }
 
-const getRandom =(min,max)=> {
+const getRandom = (min,max) => {
     return Math.round(min + Math.random() * (max - min));
 }
 
@@ -35,7 +38,7 @@ const repitsOfLetter = (letter, word) => {
 document.writeln(
     `
     Функція№1: Максимальне число з 25498 - ${getMaxDigit(25498)}</br>
-    Функція№2: Корінь з 3^3 - ${getPow(3,3)}</br>
+    Функція№2: Корінь з 3^3 - ${getPow(-9,2)}</br>
     Функція№3: Виправлення імені з АНДРІЙ - ${correctName('АНДРІЙ')}</br>
     Функція№4: Зарплата з 14000 і податком у 19.5% - ${getMoney(14000)}</br>
     Функція№5: Рандомне число з 2 по 10 - ${getRandom(2,10)}</br>
